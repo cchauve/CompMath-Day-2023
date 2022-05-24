109561ce1c4e4fd0c04686fdb4451429aca6f012
Cedric Chauve <chauvec@cedar5.cedar.computecanada.ca>
2022-05-24 14:33:55 -0700

    Installation scripts

---
+++ scripts/install_scapp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=8:00:00
+#SBATCH --mem=8G
+#SBATCH --account=def-chauvec
+
+source ../config.sh
+mkdir -p ${TOOLS_DIR}
+
+module load python/3 StdEnv/2020  gcc/9.3.0 blast+/2.12.0 bwa samtools
+python3 -m venv --system-site-packages ${TOOLS_DIR}/env_scapp
+source ${TOOLS_DIR}/env_scapp/bin/activate
+
+cd ${TOOLS_DIR}
+git clone https://github.com/Shamir-Lab/SCAPP.git
+cd SCAPP
+python setup.py install --user
+
+deactivate
