cd2a0e6c68103e25c52b69bdbadc77b2b73f4f19
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-05-25 10:10:25 -0700

    HyAsP and plASgraph scripts

---
+++ exp/plasgraph/process_samples.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1 
+#SBATCH --cpus-per-task=6 
+#SBATCH --mem=32000M       
+#SBATCH --time=1:00:00
+#SBATCH --account=def-chauvec
+#SBATCH --array=1-2
+#SBATCH --output=%A_%a.log
+#SBATCH --job-name=plasgraph
+
+# Environment variables
+source ../../config.sh
+## Sample ID
+SAMPLE_LIST=($(<${REPO_HOME}/data/samples_id.txt))
+SAMPLE=${SAMPLE_LIST[${SLURM_ARRAY_TASK_ID}]}
+## Experiment and output directory
+EXP_DIR=${REPO_HOME}/exp/plasgraph
+OUT_DIR=${EXP_DIR}/results/${SAMPLE}
+mkdir -p ${OUT_DIR}
+
+# Preparing input: assembly graph
+cp ${REPO_HOME}/data/assembly_graphs/${SAMPLE}.gfa.gz ${EXP_DIR}/tmp
+gunzip ${EXP_DIR}/tmp/${SAMPLE}.gfa.gz
+GFA=${EXP_DIR}/tmp/${SAMPLE}.gfa
+
+# Running plASgraph
+source ${TOOLS_DIR}/env_plasgraph/bin/activate
+cd ${TOOLS_DIR}/plASgraph
+mkdir -p ${OUT_DIR}
+python plASgraph.py -i ${GFA} -o ${OUT_DIR}/${SAMPLE}_class.csv --draw_graph
+
+# Cleaning
+rm ${GFA}
