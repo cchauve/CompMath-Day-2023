06e806e0afdf886b00ecad6375a3423f3030ceb7
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-05-25 16:56:33 -0700

    remove site-package option

--- scripts/install_plasclass.sh
+++ scripts/install_plasclass.sh
@@ -8,7 +8,7 @@ source ../config.sh
 mkdir -p ${TOOLS_DIR}
 
 module load python/3
-python3 -m venv --system-site-packages ${TOOLS_DIR}/env_plasclass
+python3 -m venv ${TOOLS_DIR}/env_plasclass
 source ${TOOLS_DIR}/env_plasclass/bin/activate
 pip install cython
 
--- scripts/install_scapp.sh
+++ scripts/install_scapp.sh
@@ -8,7 +8,7 @@ source ../config.sh
 mkdir -p ${TOOLS_DIR}
 
 module load python/3 StdEnv/2020  gcc/9.3.0 blast+/2.12.0 bwa samtools
-python3 -m venv --system-site-packages ${TOOLS_DIR}/env_scapp
+python3 -m venv ${TOOLS_DIR}/env_scapp
 source ${TOOLS_DIR}/env_scapp/bin/activate
 
 cd ${TOOLS_DIR}
