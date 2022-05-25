2a3a08c3ef335592f4cd5afc289172676fb17560
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-05-25 16:52:29 -0700

    running scripts

--- exp/hyasp_database_doi_10.15146_R33X2J__v2/create_database.sh
+++ exp/hyasp_database_doi_10.15146_R33X2J__v2/create_database.sh
@@ -3,6 +3,7 @@
 #SBATCH --mem=8G
 #SBATCH --account=def-chauvec
 #SBATCH --job-name=create_hyasp_db
+#SBATCH --output=create_database.log
 
 source ../../config.sh
 source ${TOOLS_DIR}/env_hyasp/bin/activate
