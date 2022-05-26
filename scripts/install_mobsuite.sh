271a600b27cdf05be7fdc2100f922cc9321390ee
Cedric Chauve <chauvec@cedar5.cedar.computecanada.ca>
2022-05-26 08:45:09 -0700

    MOB-suite installation

--- scripts/install_mobsuite.sh
+++ scripts/install_mobsuite.sh
@@ -8,7 +8,7 @@ source ../config.sh
 mkdir -p ${TOOLS_DIR}
 
 module load python/3 StdEnv/2020  gcc/9.3.0 blast+/2.12.0 mash/2.3
-python3 -m venv --system-site-packages ${TOOLS_DIR}/env_mobsuite
+python3 -m venv ${TOOLS_DIR}/env_mobsuite
 source ${TOOLS_DIR}/env_mobsuite/bin/activate
 
 pip3 install mob_suite==3.0.3
