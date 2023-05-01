9b12a2944e23688cf7ed156e6a43036a5da48d96
Cedric Chauve <chauvec@cedar5.cedar.computecanada.ca>
2023-05-01 13:08:12 -0700

    Renaming directory

--- results/2022-10-15_HyAsP/run_HyAsP_E_faecium.sh
+++ results/2022-10-15_HyAsP/run_HyAsP_E_faecium.sh
@@ -18,7 +18,7 @@ mkdir -p ${OUT_DIR}
 
 # Preparing input
 ## Reference database
-REF_DB=${REPO_HOME}/results/2022-10-01_HyAsP_database_NCBI/NCBI_plasmid_genes_db.fasta
+REF_DB=${REPO_HOME}/results/2022-10-14_HyAsP_database_NCBI/NCBI_plasmid_genes_db.fasta
 ## Sample assembly graph
 mkdir -p ${EXP_DIR}/tmp
 cp ${REPO_HOME}/data/2022-10-13_E_faecium/${SAMPLE}.gfa.gz ${EXP_DIR}/tmp/
