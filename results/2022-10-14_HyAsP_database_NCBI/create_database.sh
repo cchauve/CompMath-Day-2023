7771ebc2ae4a2e478e9580a8e6db618da80f432b
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2023-05-03 16:57:18 -0700

    db

---
+++ results/2022-10-14_HyAsP_database_NCBI/create_database.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=8:00:00
+#SBATCH --mem=8G
+#SBATCH --account=def-chauvec
+#SBATCH --job-name=create_hyasp_db
+#SBATCH --output=create_database.log
+
+source ../../config.sh
+source ${TOOLS_DIR}/env_hyasp/bin/activate
+
+# Computing the HyAsP database
+gunzip NCBI_plasmids.csv.gz
+hyasp create NCBI_plasmid_genes_db.fasta -p NCBI_plasmids.csv -b NCBI_blacklist.txt -d -l 500 -m 100 -t GenBank -v
+gzip NCBI_plasmids.csv
+
+# Summarizing the length of the sequences in the database
+${REPO_HOME}/bin/FASTA_utils.py csv_len NCBI_plasmid_genes_db.fasta NCBI_plasmid_genes_len.csv
+gzip NCBI_plasmid_genes_len.csv
